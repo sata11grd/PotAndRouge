8679b69dcc6e73679a8b5ba02879044a55067ebd
sata11grd <sata11grd@gmail.com>
2019-04-30 08:48:57 +0900

    [update]gitignore

---
+++ PotAndRouge/Assets/PotAndRouge/Audio/AudioPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace PotAndRouge.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioPlayer : SerializedMonoBehaviour
+    {
+        protected AudioSource m_AudioSource;
+
+        private void Awake()
+        {
+            m_AudioSource = GetComponent<AudioSource>();
+        }
+    }
+}
---
+++ PotAndRouge/Assets/PotAndRouge/Audio/SEPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace PotAndRouge.Audio
+{
+    public class SEPlayer : AudioPlayer
+    {
+        public void PlayOneShot(AudioClip target, float volumeScale = 0.8f)
+        {
+            Debug.Assert(m_AudioSource != null);
+            m_AudioSource.PlayOneShot(target, volumeScale);
+        }
+
+        public void PlayOneShot(float delay, AudioClip target, float volumeScale = 0.8f)
+        {
+            StartCoroutine(DelayedRun(delay, () => PlayOneShot(target, volumeScale)));
+        }
+
+        IEnumerator DelayedRun(float delay, System.Action action)
+        {
+            yield return new WaitForSeconds(delay);
+
+            action.Invoke();
+        }
+    }
+}
