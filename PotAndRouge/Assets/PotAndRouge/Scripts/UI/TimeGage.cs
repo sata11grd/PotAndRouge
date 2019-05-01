3b3079c6e1750cb5b99370fae2ce0cdd1ccd7893
sata11grd <sata11grd@gmail.com>
2019-05-01 10:51:11 +0900

    [add]time gage

---
+++ PotAndRouge/Assets/PotAndRouge/Scripts/UI/TimeGage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace PotAndRouge.UI
+{
+    public class TimeGage : SerializedMonoBehaviour
+    {
+        [OdinSerialize] Util.Timer Timer { get; set; }
+
+        Image Image { get; set; }
+
+        private void Awake()
+        {
+            Image = GetComponent<Image>();
+        }
+
+        private void Update()
+        {
+            if (Image.fillAmount == 0f) Image.gameObject.SetActive(false);
+            Image.fillAmount = Timer.RemainingDuration / Timer.Duration;
+        }
+    }
+}
