84262f0651ce6725e097ca9ecd9f5921b7ca3b8e
sata11grd <sata11grd@gmail.com>
2019-05-01 11:46:07 +0900

    [add]credit

--- PotAndRouge/Assets/Hotman/Script/Credit.cs
+++ PotAndRouge/Assets/Hotman/Script/Credit.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Credit : Button{
+    [SerializeField] UnityEngine.Events.UnityEvent OnPressed;
+
     void Start(){
         val=1;
         base.Start();
     }
     override public void Action(){
+        OnPressed.Invoke();
     }
 }
